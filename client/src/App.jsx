fa75f018903e3a12cf891c1eb93ca285b84f565d
saqibijaz48 <saqibijaz499@gmail.com>
2025-07-17 17:05:42 +0500

    com

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,67 +40,6 @@ import ContactPage from "./pages/shopping-view/contact";
 
     console.log(isLoading, user);
 
-<<<<<<< HEAD
-    return (
-      <div className="flex flex-col overflow-hidden bg-white">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <CheckAuth
-                isAuthenticated={isAuthenticated}
-                user={user}
-              ></CheckAuth>
-            }
-          />
-          <Route
-            path="/auth"
-            element={
-              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-                <AuthLayout />
-              </CheckAuth>
-            }
-          >
-            <Route path="login" element={<AuthLogin />} />
-            <Route path="register" element={<AuthRegister />} />
-          </Route>
-          <Route
-            path="/admin"
-            element={
-              // <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-                
-              // </CheckAuth>
-              <AdminLayout />
-            }
-          >
-            <Route path="dashboard" element={<AdminDashboard />} />
-            <Route path="products" element={<AdminProducts />} />
-            <Route path="orders" element={<AdminOrders />} />
-            <Route path="features" element={<AdminFeatures />} />
-          </Route>
-          <Route
-            path="/shop"
-            element={
-              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-                <ShoppingLayout />
-              </CheckAuth>
-            }
-          >
-            <Route path="home" element={<ShoppingHome />} />
-            <Route path="listing" element={<ShoppingListing />} />
-            <Route path="checkout" element={<ShoppingCheckout />} />
-            <Route path="account" element={<ShoppingAccount />} />
-            <Route path="paypal-return" element={<PaypalReturnPage />} />
-            <Route path="payment-success" element={<PaymentSuccessPage />} />
-            <Route path="search" element={<SearchProducts />} />
-          </Route>
-          <Route path="/unauth-page" element={<UnauthPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    );
-  }
-=======
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
@@ -163,6 +102,5 @@ import ContactPage from "./pages/shopping-view/contact";
     </div>
   );
 }
->>>>>>> d3b05c46d4cd2632038015eb9b3d45202ec9e5b8
 
   export default App;
